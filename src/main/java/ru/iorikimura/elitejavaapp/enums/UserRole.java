600179b76da988a8ab3e86fadc3d74270c18630e
IoriKimura <sergeykonstwork@gmail.com>
2023-11-21 15:37:32 +0300

    Заготовка для Spring Security и добавлен ENUM класс со статусами оплаты.

--- src/main/java/ru/iorikimura/elitejavaapp/enums/UserRole.java
+++ src/main/java/ru/iorikimura/elitejavaapp/enums/UserRole.java
@@ -3,5 +3,11 @@ package ru.iorikimura.elitejavaapp.enums;
 public enum UserRole {
     USER,
     ADMIN,
-    EVENT_OWNER
+    EVENT_OWNER;
+
+    // Заготовка на будущий Spring Security
+//    @Override
+//    public String getAuthority(){
+//        return this.name();
+//    }
 }
